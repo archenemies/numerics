ca37022b517b06e952b57913ce54ad2d1959147f
Frederick Eaton <frederik@ofb.net>
2025-03-25 13:47:57 -0700

    Add sync_mduals

--- mdual-number.R
+++ mdual-number.R
@@ -1,6 +1,8 @@
 # FHE 24 Mar 2025
 # https://grok.com/chat/c7aace3d-072c-4f47-aa28-f5a1923dfdfa?show_subscribe=0
 
+mysource("lastind.R")
+
 mdual_number <- function(value, mdual, names) {
   # Validate inputs
   stopifnot(is.numeric(value))
@@ -26,6 +28,35 @@ mdual_number <- function(value, mdual, names) {
 
   # Create and return the object
   structure(list(value = value, mdual = mdual, names = names), class = "mdual_number")
+}
+
+is.mdual = function(x) { inherits(x, "mdual_number") }
+
+stripClass = function(x) {
+  class(x) = NULL
+  x
+}
+
+format.mdual_number = function(x) {
+  cat("mdual:\n")
+  format(stripClass(x))
+}
+
+print.mdual_number = function(x) {
+  ns = x$names
+  cat("mdual_number: ", paste0(ns, collapse=","), "\n")
+  cat("  VALUE=\n")
+  print(x$value)
+  if(0) {
+    cat("  MDUAL=\n")
+    print(x$mdual)
+  } else {
+    stopifnot(length(ns) == lastDim(x$mdual))
+    for(i in seq_along(ns)) {
+      cat("  DUAL[",i,"]= (",ns[i],")\n",sep="")
+      print(lastInd(x$mdual, i))
+    }
+  }
 }
 
 # helper function to append 1 to dimension list
@@ -72,28 +103,37 @@ promote_mdual = function(mx, new_names) {
   mx
 }
 
-if(1) {
-# tests:
-x = mdual_number(1, array(1), "dt")
-promote_mdual(x, c("du","dt"))
-
-arr = array(1:24, c(2,3,4))
-ax = mdual_number(arr, vector_of(arr), "dt")
-promote_mdual(ax, c("dt","du"))
+# sync_mduals: promote each mdual_number the list mds
+# input: list of mdual_numbers
+# output: same list but promoted to have same $names
+sync_mduals = function(mds) {
+  stopifnot(is.list(mds))
+  all_names=list()
+  for(md in mds) {
+    all_names = union(all_names, md$names)
+  }
+  # (for some reason union makes it a list:)
+  all_names = as.character(all_names)
+  res_mds = list()
+  for(i in seq_along(mds)) {
+    res_mds[[i]] = promote_mdual(mds[[i]], all_names)
+  }
+  res_mds
 }
 
-# - sync_mduals
-#   - input: list of mdual_number's.
-#     output: same list but promoted to have same names
-#   - call from op method
-# \- rewrite to make $names the last dimension (rather than first) in $mdual
-# \- in promote_mdual
-#   \- use match() to get indices of new names
-#   \- turn $mdual into matrix by setting dim()
-#     /- need helper function for this?
-#   \- use cbind to add zeroes, then the missing variables just refer to this zero column
-#   \- we can then reindex like this > m[1:2,c(2,3,1,3)]
-# - fill in the rest from dual-number.R
 
-## # - use slice.index(x, 1) to get indices into $names
-## #     and use slice.index(x, c(2,3)) to get indices into $value
+if(1) {
+  # tests:
+  x = mdual_number(1, array(1), "dt")
+  promote_mdual(x, c("du","dt"))
+
+  arr = array(1:24, c(2,3,4))
+  ax = mdual_number(arr, vector_of(arr), "dt")
+  promote_mdual(ax, c("dt","du"))
+
+  y = mdual_number(4, array(1), "du")
+  s = sync_mduals(list(x,y))
+}
+
+# - call sync_mduals from op method
+# - fill in the rest from dual-number.R
