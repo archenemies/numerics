294a8e86dc6d79cce270ad95b075262647028130
Frederick Eaton <frederik@ofb.net>
2025-03-25 12:51:43 -0700

    Rename and fix double evaluation problem

--- num-wrap.R
+++ num-wrap.R
@@ -3,10 +3,13 @@
 # edited by adding eval.parent call to get it to work
 # was test-num-wrapper-2.R
 
-# define the num_wrapper class
-num_wrapper <- function(value) {
-  structure(list(value = value), class = "num_wrapper")
+# define the num_wrap class
+num_wrap <- function(value) {
+  message("in num_wrap: ",deparse(value))
+  structure(list(value = value), class = "num_wrap")
 }
+
+is.num_wrap = function(x) { inherits(x, "num_wrap") }
 
 # list of operators/functions to override
 basic_ops <- c("+", "*", "-", "/", "t", "%*%", "solve")
@@ -17,18 +20,30 @@ create_method <- function(op) {
   op_func <- get(op)
   
   # the method name
-  method_name <- paste0(op, ".num_wrapper")
+  method_name <- paste0(op, ".num_wrap")
   
   # define the wrapper function. we'll fill in the argument names
   # later
-  wrapper_func <- function() {
+  wrapper_func <- function(...) {
     # get the arguments passed to the function as a list:
-    args <- as.list(match.call())[-1] # -1: exclude the function name itself
-    args <- lapply(args, eval.parent)
+
+    # match.call gives us language objects for some reason, and we
+    # couldn't figure out how to use these without creating a double
+    # evaluation
+
+#    lang_args <- as.list(match.call())[-1] # -1: exclude the function name itself
+##    args <- lapply(lang_args, eval.parent)
+##    args <- do.call(list, lang_args)
+
+#    message("HERE A");
+
+    args <- list(...);
+
+#    message("HERE B");
+
     unwrapped_args <- lapply(args, function(arg) {
-      # match.call gives us language objects for some reason,
       # must use eval:
-      if (inherits(arg, "num_wrapper")) {
+      if (is.num_wrap(arg)) {
         arg$value
       } else {
         arg
@@ -37,11 +52,11 @@ create_method <- function(op) {
     # now call op_func
     result <- do.call(op_func, unwrapped_args)
     # and return the wrapped result
-    num_wrapper(result)
+    num_wrap(result)
   }
   
   # fill in the arguments of wrapper_func to match op_func
-  formals(wrapper_func) <- formals(args(op_func))
+#  formals(wrapper_func) <- formals(args(op_func))
   
   # now install the wrapper function
   assign(method_name, wrapper_func, envir = .GlobalEnv)
@@ -49,4 +64,9 @@ create_method <- function(op) {
 
 for (op in basic_ops) {
   create_method(op)
+}
+
+if(0) {
+  e1 = num_wrap(1); e2 = num_wrap(2); e1+e2
+  num_wrap(1)+num_wrap(2)
 }
