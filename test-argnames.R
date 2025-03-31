4504d0fa899b4c114b7e925e141ce7a705cae647
Frederick Eaton <frederik@ofb.net>
2025-03-30 17:07:46 -0700

    Move old stuff

---
+++ test-argnames.R
@@ -0,0 +1,10 @@
+
+func <- function(...) {
+  args <- list(...);
+  arg_names = lapply(substitute(...()),deparse)
+  lapply(seq_along(args), function(i) {
+    a = args[[i]]
+    n = arg_names[[i]]
+    pv(a,n)
+  })
+}
--- test.R
+++ test.R
@@ -1,9 +1,1 @@
-func <- function(...) {
-  args <- list(...);
-  arg_names = lapply(substitute(...()),deparse)
-  lapply(seq_along(args), function(i) {
-    a = args[[i]]
-    n = arg_names[[i]]
-    pv(a,n)
-  })
-}
+
