3bc7a0b657cc4b8dc43610a12d8e48d2d054d405
Frederick Eaton <frederik@ofb.net>
2025-04-05 14:43:22 -0700

    Fix deparse() printing numbers that are too long

--- generic-deparse.R
+++ generic-deparse.R
@@ -12,3 +12,18 @@ deparse.default <- function(...) {
 deparse <- function(...) {
   UseMethod("deparse")
 }
+
+# our deparse for numerics. shorten numbers to 4 decimal places. since
+# base::deparse is doing complicated stuff like deciding when to write
+# "c()" and when to write "1:4", it seems easier to just manipulate
+# the string it returns than to try to duplicate its functionality.
+deparse.numeric = function(x,...) {
+    str = base::deparse(x,...);
+    gsub("(\\.\\d\\d\\d\\d)\\d+","\\1", str)
+}
+
+# from R-4.2.0/src/library/base/R/utilities.R
+## deparse(.) returning *one* string
+deparse1 <- function(expr, collapse = " ", width.cutoff = 500L, ...)
+    paste(deparse(expr, width.cutoff, ...), collapse=collapse)
+
