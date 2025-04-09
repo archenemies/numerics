6e79cbd28869ead555692474ea07367270fa85a8
Frederick Eaton <frederik@ofb.net>
2025-04-08 18:31:21 -0700

    Cleanup comments

--- generic-ops.R
+++ generic-ops.R
@@ -1,24 +1,12 @@
 # FHE 06 Apr 2025
 # create generic methods
 
-## rowSums.default <- function(...) { base::rowSums(...) }
-## rowSums <- function(...) { UseMethod("rowSums") }
-
-## colSums.default <- function(...) { base::colSums(...) }
-## colSums <- function(...) { UseMethod("colSums") }
-
-## sum.default <- function(...) { base::sum(...) }
-## sum <- function(...) { UseMethod("sum") }
-
-## exp.default <- function(...) { base::exp(...) }
-## exp <- function(...) { UseMethod("exp") }
-
+# example generic method pair:
 ## log.default <- function(...) { base::log(...) }
 ## log <- function(...) { UseMethod("log") }
 
 convertToGeneric <- function(fname) {
   base_fn = getFromNamespace(fname, "base")
-#  base_fn = get(paste0("base::", fname))
   assign(paste0(fname, ".default"),
          base_fn,
          envir = .GlobalEnv)
@@ -30,6 +18,6 @@ convertToGeneric <- function(fname) {
 funs <- list("rowSums", "colSums", "sum", "exp", "log",
   "array")
 
-# these already seem to be generic: "rep", "as.vector", "length"
+# the following already seem to be generic: "rep", "as.vector", "length"
 
 lapply(funs, convertToGeneric)
