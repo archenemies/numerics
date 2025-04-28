8b1097bd99363adb28d05d5ed0fae8f2e56d60d0
Frederick Eaton <frederik@ofb.net>
2025-04-28 16:51:23 -0700

    Test loading word vectors from rt/wordvec. TODO items and cleanup.

---
+++ test-load-wordvecs.R
@@ -0,0 +1,21 @@
+
+#prefix = "../wordvecs/gn300-wk"
+prefix = "../wordvecs/gn300-wk-short"
+
+header_fn = paste0(prefix, ".hdr")
+wordvec_fn = paste0(prefix, ".wvs")
+table_fn = paste0(prefix, ".tab")
+
+header = as.numeric(read.table(header_fn))
+vector_length = header[1]
+vocab_size = header[2]
+
+wvconn = file(wordvec_fn, "rb")
+wvs = readBin(wvconn, numeric(), size=4, n=vocab_size*vector_length)
+dim(wvs_raw) <- c(vocab_size, vector_length)
+
+wtab = read.table(table_fn, quote=NULL, col.names=c("index","binindex","word"))
+
+
+# this takes too long
+#wvs = matrix(wvs_raw, ncol=vector_length)
